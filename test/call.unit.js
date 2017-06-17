76214ac5562eff97422282a85711db53bea2a98e
max@mlg.tv <mexxik@gmail.com>
2017-06-17 11:51:08 -0700

    call function fixed

--- test/call.unit.js
+++ test/call.unit.js
@@ -1,7 +1,25 @@
 var RPC     = require('./../index');
 
 exports.group = {
-    testCall: function(test) {
+    testCall1: function(test) {
+        var number = Math.random();
+
+        var receiver = RPC.create();
+        receiver.connect();
+        receiver.on('ready', function() {
+            receiver.register('simpleCall', function(response) {
+                test.ok(response === number, 'sent and received numbers match');
+                test.done();
+            });
+
+            var sender = RPC.create();
+            sender.connect();
+            sender.on('ready', function() {
+                sender.call('simpleCall', number, {}, function(){});
+            });
+        });
+    },
+    testCall2: function(test) {
         var number = Math.random();
 
         var receiver = RPC.create();
@@ -16,6 +34,24 @@ exports.group = {
             sender.connect();
             sender.on('ready', function() {
                 sender.call('simpleCall', number, function(){});
+            });
+        });
+    },
+    testCall3: function(test) {
+        var number = Math.random();
+
+        var receiver = RPC.create();
+        receiver.connect();
+        receiver.on('ready', function() {
+            receiver.register('simpleCall', function(response) {
+                test.ok(response === number, 'sent and received numbers match');
+                test.done();
+            });
+
+            var sender = RPC.create();
+            sender.connect();
+            sender.on('ready', function() {
+                sender.call('simpleCall', number);
             });
         });
     }
